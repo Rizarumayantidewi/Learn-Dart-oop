041e73485562d4aee90c876990c3191cafba3dea
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2022-02-22 20:39:34 +0700

    Add files via upload

---
+++ bin/encapsulation.dart
@@ -0,0 +1,4 @@
+//artinya memastikan data sensitif sebuah object tersembunyi dari akses luar
+//biasanya kita akan membuat semua field yang tidak bisa diakses dari luar
+//agar bisa diubah, menyediakan method untuk mengubah dan mendapatkan field tersebut
+//proses encapsulation sdh dibuat standarisasinya, gunakan Getter&Setter
